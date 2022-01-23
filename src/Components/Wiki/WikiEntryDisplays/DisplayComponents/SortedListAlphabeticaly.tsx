8f1a9fc51645fc124d20e14b0ef25c931a651677
guzicco <zawilski.michaal@gmail.com>
2022-01-23 15:50:13 +0100

    extracted sortedLIst for reuseability changed display from accodion to list

---
+++ src/Components/Wiki/WikiEntryDisplays/DisplayComponents/SortedListAlphabeticaly.tsx
@@ -0,0 +1,51 @@
+import { Link, List, ListItem, Paper } from "@mui/material";
+import { ILink } from "../../../../App";
+import { useWikiDataHandler } from "../../WikiContext";
+import { EQCategories } from "../DisplayEquipmentCategories";
+
+interface IProps {
+  itemsList: EQCategories;
+}
+
+const SortedListAlphabeticaly: React.FC<IProps> = ({ itemsList }) => {
+  const wikiDataHandler = useWikiDataHandler();
+  const sortAlphabetUniqueLetters = (list: ILink[]) => {
+    let uniqueLetters = new Set<string>();
+    list.map((item) => uniqueLetters.add(item.name.charAt(0)));
+    return Array.from(uniqueLetters).sort();
+  };
+  const sortListByLetter: (letter: string) => ILink[] = (letter) => {
+    return itemsList.equipment.filter(
+      (item) => item.name.charAt(0) === letter
+    ) as ILink[];
+  };
+
+  const uniqueLetters = sortAlphabetUniqueLetters(itemsList.equipment);
+
+  return (
+    <Paper>
+      {uniqueLetters.map((letter: string) => {
+        return (
+          <List key={letter} sx={{ pl: 2 }}>
+            {letter}
+            {sortListByLetter(letter).map((entry) => (
+              <ListItem key={entry.index}>
+                <Link
+                  component="button"
+                  variant="body1"
+                  onClick={() => {
+                    wikiDataHandler.onInItemLinkClick(entry.url);
+                  }}
+                >
+                  {entry.name}
+                </Link>
+              </ListItem>
+            ))}
+          </List>
+        );
+      })}
+    </Paper>
+  );
+};
+
+export default SortedListAlphabeticaly;
--- src/Components/Wiki/WikiEntryDisplays/DisplayEquipmentCategories.tsx
+++ src/Components/Wiki/WikiEntryDisplays/DisplayEquipmentCategories.tsx
@@ -1,59 +1,27 @@
-import { Box, CardHeader, Link, List, ListItem, Paper } from "@mui/material";
+import { Card, CardContent, CardHeader } from "@mui/material";
 import { ILink } from "../../../App";
-import { EWikiStates, useWikiData, useWikiDataHandler } from "../WikiContext";
+import { EWikiStates, useWikiData } from "../WikiContext";
 import { assertState } from "../../../Utils/AssertState";
+import SortedListAlphabeticaly from "./DisplayComponents/SortedListAlphabeticaly";
 
-interface EQCategories extends ILink {
+export interface EQCategories extends ILink {
   equipment: ILink[];
 }
 
 const DisplayEquipmentCategories: React.FC = () => {
   const wikiData = useWikiData();
-  const wikiDataHanlder = useWikiDataHandler();
-
   assertState(wikiData, EWikiStates.ITEM_PICKED);
   const entryData: EQCategories = wikiData.itemPicked;
 
-  console.log(entryData);
-
-  const sortAlphabetUniqueLetters = (list: ILink[]) => {
-    let uniqueLetters = new Set<string>();
-    list.map((item) => uniqueLetters.add(item.name.charAt(0)));
-    return Array.from(uniqueLetters).sort();
-  };
-  const sortListByLetter: (letter: string) => ILink[] = (letter) => {
-    return entryData.equipment.filter(
-      (item) => item.name.charAt(0) === letter
-    ) as ILink[];
-  };
-  const uniqueLetters = sortAlphabetUniqueLetters(entryData.equipment);
-
   return (
-    <>
+    <Card>
       <CardHeader title={entryData.name}></CardHeader>
-      <Box>
-        <Paper>
-          {uniqueLetters.map((letter: string) => {
-            return (
-              <List key={letter} sx={{ pl: 2 }}>
-                {letter}
-                {sortListByLetter(letter).map((entry) => (
-                  <ListItem key={entry.index}>
-                    <Link
-                      onClick={() => {
-                        wikiDataHanlder.onInItemLinkClick(entry.url);
-                      }}
-                    >
-                      {entry.name}
-                    </Link>
-                  </ListItem>
-                ))}
-              </List>
-            );
-          })}
-        </Paper>
-      </Box>
-    </>
+      <CardContent>
+        <SortedListAlphabeticaly
+          itemsList={entryData}
+        ></SortedListAlphabeticaly>
+      </CardContent>
+    </Card>
   );
 };
 
